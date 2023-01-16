f13511d41e8c26125ed10d331b2b4f1c4406eea6
Apple Macbook Air M1 <applemacbookairm1@Apples-MacBook-Air.local>
2023-01-16 12:56:16 +0400

    pr comments are solved

--- core/api/views.py
+++ core/api/views.py
@@ -15,9 +15,3 @@ class NewsList(APIView):
         serializer = NewsSerializer(news, many=True)
         return Response(serializer.data)
 
-    def post(self, request, format=None):
-        serializer = NewsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
