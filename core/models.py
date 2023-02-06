7b70acda5c715a313f4f06aff72150f8f9875c4f
Apple Macbook Air M1 <applemacbookairm1@Apples-MacBook-Air.local>
2023-02-06 14:54:55 +0400

    Reverting to 2646698

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ import datetime
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from account.models import CustomUser
+from core.abstracts import SingletonModel
 class NewsCategory(models.Model):
 
     title = models.CharField(max_length=50, blank=False)
@@ -43,23 +44,57 @@ class Country(models.Model):
     def __str__(self):
         return self.name
     
+class FAQ_Category(models.Model):
 
-class SingletonModel(models.Model):
+    name = models.CharField(_('name'), max_length=100)
+    sort_order = models.IntegerField(_('sort order'), default=0, help_text=_('The order you would like the faq questions to be displayed.'))
+
     class Meta:
-        abstract = True
+        verbose_name = _("FAQ Category")
+        verbose_name_plural = _("FAQ Categories")
+        ordering = ['sort_order', 'name']
 
-    def save(self, *args, **kwargs):
-        self.pk = 1
-        super(SingletonModel, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.name
+   
+class FAQ(models.Model):
 
-    def delete(self, *args, **kwargs):
-        pass
 
-    @classmethod
-    def load(cls):
-        obj, created = cls.objects.get_or_create(pk=1)
-        return obj
+    ACTIVE = 1
+    INACTIVE = 0
+    STATUS_CHOICES = (
+        (ACTIVE,    _('Active')),
+        (INACTIVE,  _('Inactive')),
+        )
+    
+    
+    question = models.TextField(_('question'))
+    answer = models.TextField(_('answer'), blank=True)
+    faq_category = models.ForeignKey(FAQ_Category, verbose_name=_('faq category'), related_name='questions')
+    slug = models.SlugField(_('slug'), max_length=100)
+    sort_order = models.IntegerField(_('sort order'), default=0, help_text=_('The order you would like the question to be displayed.'))
+    status = models.IntegerField(_('status'),
+        choices=STATUS_CHOICES, default=INACTIVE)
+    
+    created_on = models.DateTimeField(_('created on'), default=datetime.datetime.now)
+    updated_on = models.DateTimeField(_('updated on'))
+    created_by = models.ForeignKey(CustomUser, verbose_name=_('created by'),
+        null=True)
+    updated_by = models.ForeignKey(CustomUser, verbose_name=_('updated by'),
+        null=True)    
+    
 
+
+    class Meta:
+        verbose_name = _("Frequent asked question")
+        verbose_name_plural = _("Frequently asked questions")
+        ordering = ['sort_order', 'created_on']
+
+
+    def __str__(self):
+        return self.question
+
+    
 class ContactUs(SingletonModel):
     email = models.EmailField()
     phone = models.CharField(max_length=20) 
