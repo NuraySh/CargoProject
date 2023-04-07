2155a7e8c14158cd9f23f359df79d252c0312448
Apple Macbook Air M1 <applemacbookairm1@Apples-MacBook-Air.local>
2023-04-07 14:34:11 +0400

    clean method is modified, unmigrate migrate is done

--- account/models.py
+++ account/models.py
@@ -1,22 +1,24 @@
-from django.db import models
 import re
+
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from account.managers import CustomUserManager
-from account.helpers import id_gen
-from django.utils.translation import gettext_lazy as _
-from account.validators import validate_phone, validate_pin_code
 from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from account.helpers import id_gen
+from account.managers import CustomUserManager
+from account.validators import validate_phone, validate_pin_code
 
 
 class PhonePrefix(models.Model):
-
     PREFIXES = [
-        ('+99455', '+99455'),
-        ('+99450', '+99450'),
-        ('+99451', '+99451'),
-        ('+99470', '+99470'),
-        ('+99477', '+99477'),
-        ('+99499', '+99499')]
+        ("+99455", "+99455"),
+        ("+99450", "+99450"),
+        ("+99451", "+99451"),
+        ("+99470", "+99470"),
+        ("+99477", "+99477"),
+        ("+99499", "+99499"),
+    ]
 
     prefix_number = models.CharField(max_length=6, choices=PREFIXES)
 
@@ -25,11 +27,7 @@ class PhonePrefix(models.Model):
 
 
 class Warehouse(models.Model):
-    BRANCHES = [
-        ('GN', 'Ganjlik'),
-        ('NR',  'Narimanov'),
-        ('20Jan',  '20 January')
-    ]
+    BRANCHES = [("GN", "Ganjlik"), ("NR", "Narimanov"), ("20Jan", "20 January")]
     name = models.CharField(max_length=5, choices=BRANCHES, blank=False, null=False)
 
     def __str__(self):
@@ -37,69 +35,65 @@ class Warehouse(models.Model):
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name="email address", max_length=255, unique=True, null=True
+    )
+    first_name = models.CharField(max_length=25, verbose_name="first name")
+    last_name = models.CharField(max_length=25, verbose_name="first name")
 
-    is_cleaned = False
+    GENDER = [("m", "male"), ("f", "female")]
 
-    email = models.EmailField(verbose_name='email address', max_length=255, unique=True, null=True)
-    first_name = models.CharField(max_length=25, verbose_name='first name')
-    last_name = models.CharField(max_length=25, verbose_name='first name')
-
-    GENDER = [
-        ('m', 'male'),
-        ('f', 'female')]
-    
     gender = models.CharField(max_length=1, choices=GENDER)
 
     phone_prefix = models.ForeignKey(PhonePrefix, on_delete=models.CASCADE, null=True)
     phone = models.CharField(max_length=7, validators=[validate_phone])
-    SERIES = [
-        ('AZE', 'AZE'),
-        ('AA', 'AA'),
-        ('MYI', 'MYI'),
-        ('DYI', 'DYI')
-    ]
-    gov_id_prefix = models.CharField(max_length=3, choices=SERIES, default='AZE')
+
+    SERIES = [("AZE", "AZE"), ("AA", "AA"), ("MYI", "MYI"), ("DYI", "DYI")]
+
+    gov_id_prefix = models.CharField(max_length=3, choices=SERIES, default="AZE")
     gov_id = models.CharField(max_length=8, unique=True)
-    pin_code = models.CharField(max_length=7, unique=True, validators=[validate_pin_code]) 
-    client_code = models.CharField(max_length=9, primary_key=True, default=id_gen, editable=False) #have to change
+    pin_code = models.CharField(
+        max_length=7, unique=True, validators=[validate_pin_code]
+    )
+    client_code = models.CharField(
+        max_length=9, primary_key=True, default=id_gen, editable=False
+    )
     monthly_expense = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
     birth_date = models.DateField(null=True)
-    branch = models.ForeignKey(Warehouse, on_delete=models.CASCADE, null=True) 
+    branch = models.ForeignKey(Warehouse, on_delete=models.CASCADE, null=True)
     is_active = models.BooleanField(default=False)
     is_blocked = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['birth_date']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["birth_date"]
 
     objects = CustomUserManager()
 
     def __str__(self):
         return self.email
-    
+
     def clean(self):
         super().clean()
-        
-        # Validate gov_id length based on gov_id_prefix
+
+        regex1 = "^\d{8}$"
+        regex2 = "^\d{7}$"
+        regex3 = "^(?:\d{5}|\d{6})$"
+        validators = {"AZE": regex1, "AA": regex2, "MYI": regex3, "DYI": regex3}
         prefix = self.gov_id_prefix
-        gov_id_length = len(self.gov_id)
-        
-        if prefix == 'AZE' and gov_id_length != 8:
-            raise ValidationError("The gov_id field length must be 8 for AZE prefix.")
-        elif prefix == 'AA' and gov_id_length != 7:
-            raise ValidationError("The gov_id field length must be 7 for AA prefix.")
-        elif prefix == 'MYI' and (gov_id_length != 6 and gov_id_length != 5):
-            raise ValidationError("The gov_id field length must be 5 or 6 for MYI prefix.")
-        elif prefix == 'DYI' and (gov_id_length != 6 and gov_id_length != 5):
-            raise ValidationError("The gov_id field length must be 5 or 6 for DYI prefix.")
-    
+
+        if re.match("^[0-9]+$", self.gov_id):
+            if not re.match(validators[prefix], self.gov_id):
+                raise ValidationError("Please enter right numbers of digits")
+        else:
+            raise ValidationError("All numbers have to be digit")
+
     def save(self, *args, **kwargs):
         self.full_clean()
         super(CustomUser, self).save(*args, **kwargs)
 
-
     def get_full_name(self):
-        full_name = '%s %s' % (self.first_name, self.last_name)
+        full_name = "%s %s" % (self.first_name, self.last_name)
         return full_name.strip()
 
     def get_short_name(self):
--- account/tests.py
+++ account/tests.py
@@ -1,71 +1,69 @@
-import unittest
 import re
-from django.core.exceptions import ValidationError
-from account.factories import CustomUserFactory
-import django
-from account.models import CustomUser
-class TestCustomUserValidators(django.test.TestCase):
+import unittest
 
-     def test_valid_user_creation(self):
+import django
+from django.core.exceptions import ValidationError
+
+from account.factories import CustomUserFactory
+from account.models import CustomUser
+
+
+class TestCustomUserValidators(django.test.TestCase):
+    def test_valid_user_creation(self):
         user = CustomUserFactory()
         self.assertEqual(CustomUser.objects.count(), 1)
         self.assertEqual(user.is_active, False)
-    
-     def test_phone_valid_user_creation(self):
-        user = CustomUserFactory(phone = '5899298')
+
+    def test_phone_valid_user_creation(self):
+        user = CustomUserFactory(phone="5899298")
         self.assertEqual(CustomUser.objects.count(), 1)
         self.assertEqual(user.is_active, False)
 
-     def test_phone_invalid_user_creation_phone(self):
-         with self.assertRaises(ValidationError):
-            CustomUserFactory.create(phone = '589929')
-
-     def test_pincode_valid_user_creation(self):
-        user = CustomUserFactory(pin_code = '5zxu4pf')
-        self.assertEqual(CustomUser.objects.count(), 1)
-        self.assertEqual(user.is_active, False)
-    
-     def test_invalid_user_creation_pincode(self):
+    def test_phone_invalid_user_creation_phone(self):
         with self.assertRaises(ValidationError):
-            CustomUserFactory.create(pin_code = '5zxu4pst')
+            CustomUserFactory.create(phone="589929")
 
-
-     def test_govid_valid_user_creation_govid_AZE(self):
-        user = CustomUserFactory(gov_id_prefix = 'AZE', gov_id = '12345678')
+    def test_pincode_valid_user_creation(self):
+        user = CustomUserFactory(pin_code="5zxu4pf")
         self.assertEqual(CustomUser.objects.count(), 1)
         self.assertEqual(user.is_active, False)
 
-     def test_gov_id_invalid_user_creation_govid_AZE(self):
-         with self.assertRaises(ValidationError):
-             CustomUserFactory.create(gov_id_prefix='AZE', gov_id ='1234567')
-    
-     def test_govid_valid_user_creation_govid_AA(self):
-        user = CustomUserFactory(gov_id_prefix = 'AA', gov_id = '1234567')
+    def test_invalid_user_creation_pincode(self):
+        with self.assertRaises(ValidationError):
+            CustomUserFactory.create(pin_code="5zxu4pst")
+
+    def test_govid_valid_user_creation_govid_AZE(self):
+        user = CustomUserFactory(gov_id_prefix="AZE", gov_id="12345678")
         self.assertEqual(CustomUser.objects.count(), 1)
         self.assertEqual(user.is_active, False)
 
-     def test_gov_id_invalid_user_creation_govid_AA(self):
-         with self.assertRaises(ValidationError):
-             CustomUserFactory.create(gov_id_prefix = 'AA', gov_id ='12345678')
+    def test_gov_id_invalid_user_creation_govid_AZE(self):
+        with self.assertRaises(ValidationError):
+            CustomUserFactory.create(gov_id_prefix="AZE", gov_id="1234567")
 
-     def test_govid_valid_user_creation_govid_MYI(self):
-        user = CustomUserFactory(gov_id_prefix = 'MYI', gov_id = '123456')
+    def test_govid_valid_user_creation_govid_AA(self):
+        user = CustomUserFactory(gov_id_prefix="AA", gov_id="1234567")
         self.assertEqual(CustomUser.objects.count(), 1)
         self.assertEqual(user.is_active, False)
 
-     def test_gov_id_invalid_user_creation_govid_MYI(self):
-         with self.assertRaises(ValidationError):
-             CustomUserFactory.create(gov_id_prefix = 'MYI', gov_id ='1234567')
+    def test_gov_id_invalid_user_creation_govid_AA(self):
+        with self.assertRaises(ValidationError):
+            CustomUserFactory.create(gov_id_prefix="AA", gov_id="12345678")
 
-     def test_govid_valid_user_creation_govid_DYI(self):
-        user = CustomUserFactory(gov_id_prefix = 'DYI', gov_id = '12345')
+    def test_govid_valid_user_creation_govid_MYI(self):
+        user = CustomUserFactory(gov_id_prefix="MYI", gov_id="123456")
         self.assertEqual(CustomUser.objects.count(), 1)
         self.assertEqual(user.is_active, False)
 
-     def test_gov_id_invalid_user_creation_govid_DYI(self):
-         with self.assertRaises(ValidationError):
-             CustomUserFactory.create(gov_id_prefix = 'DYI', gov_id ='1234567')
-         
-         
-     
+    def test_gov_id_invalid_user_creation_govid_MYI(self):
+        with self.assertRaises(ValidationError):
+            CustomUserFactory.create(gov_id_prefix="MYI", gov_id="1234567")
 
+    def test_govid_valid_user_creation_govid_DYI(self):
+        user = CustomUserFactory(gov_id_prefix="DYI", gov_id="12345")
+        self.assertEqual(CustomUser.objects.count(), 1)
+        self.assertEqual(user.is_active, False)
+
+    def test_gov_id_invalid_user_creation_govid_DYI(self):
+        with self.assertRaises(ValidationError):
+            CustomUserFactory.create(gov_id_prefix="DYI", gov_id="1234567")
